f81f221d47f92288e698ed9a469bace5ac556383
YUN_SANGMIN <74187642+redtea89@users.noreply.github.com>
2022-05-27 20:07:22 +0900

    ADD: model 추가

--- apps/madup/models.py
+++ apps/madup/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertiser(models.Model):
+    """
+    광고주 데이터
+    """
+    name = models.CharField(max_length=30)
+    email = models.EmailField(default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class AdvertisingData(models.Model):
+    """
+    광고주를 FK로 설정한 광고데이터
+    """
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+    uid = models.CharField(max_length=100)
+    media = models.CharField(max_length=30)
+    date = models.DateField()
+    cost = models.IntegerField()
+    impression = models.IntegerField()
+    click = models.IntegerField()
+    conversion = models.IntegerField()
+    cv = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
