3a3868ee1e77f17f11e78a117e5b1d09b6fc63cb
YUN_SANGMIN <74187642+redtea89@users.noreply.github.com>
2022-06-02 16:58:06 +0900

    ADD: Kpi Per Restaurant API 작성

--- apps/bear/filters.py
+++ apps/bear/filters.py
@@ -1,14 +1,81 @@
 from datetime import datetime
+from datetime import timedelta
+
+from django.db.models import Q, Sum, F
+from django.db.models.functions import (
+    ExtractHour, ExtractDay, ExtractWeek ,ExtractMonth, ExtractYear,
+    TruncDate, TruncWeek,
+)
 
 from rest_framework.filters import BaseFilterBackend
 from rest_framework.exceptions import ValidationError
 
+from .models import Pos, Group
+
 
 class KpiPerRestaurantFilter(BaseFilterBackend):
-    """
-    각 필터링을 적용하는 함수 넣기
-    """
-    pass
+    def _get_params(self, request):
+            """
+            Get params
+            - params = "time_window", "number_of_party", "start_time", "end_time"
+                "start_price", "end_price", "restaurant_name"
+            """
+            time_window = request.GET.get('time_window', None)
+            start_time = request.GET.get('start_time', None)
+            end_time = request.GET.get('end_time', None)
+            start_price = request.GET.get('start_price', None)
+            end_price = request.GET.get('end_price', None)
+            restaurant_group = request.GET.get('restaurant_group', None)
+            if time_window is None:
+                raise ValidationError("time_window를 입력하세요.")
+            if start_time is None:
+                raise ValidationError("start_time를 입력하세요.")
+            if end_time is None:
+                raise ValidationError("end_time를 입력하세요.")
+            
+            # 날짜 포맷변환
+            date_format = '%Y-%m-%d'
+            end_time = datetime.strptime(end_time, date_format)
+            end_time = end_time.date()
+
+            params = {
+                "time_window": time_window,
+                "start_time": start_time,
+                "end_time": end_time,
+                "start_price": start_price,
+                "end_price": end_price,
+                "restaurant_group": restaurant_group
+            }
+            return params
+
+    def filter_queryset(self, request, queryset, view):
+        """
+        param 조건으로 ORM Filtering 적용
+        """
+        params = self._get_params(request)
+        time_windows = {
+            'hour' : ExtractHour('timestamp'),
+            'day'  : TruncDate('timestamp'),
+            'week' : TruncWeek('timestamp'),
+            'month': ExtractMonth('timestamp'),
+            'year' : ExtractYear('timestamp')
+        }
+        
+        if params['restaurant_group'] is not None:
+            group = Group.objects.filter(name=params['restaurant_group']).first()
+
+        
+        q = Q()
+        q &= Q(timestamp__range=(params['start_time'], params['end_time'] + timedelta(days=1)))
+        if params['restaurant_group'] is not None:
+            q &= Q(restaurant__group_id__exact=group)
+        if (params['start_price'] is not None) and (params['end_price'] is not None):
+            q &= Q(price__range=(int(params['start_price']), int(params['end_price'])))
+
+        objs = Pos.objects.filter(q)\
+                    .annotate(date=time_windows[params['time_window']]).values('date')\
+                    .annotate(total_price=Sum('price'))
+        return objs
 
 
 class KpiPerPaymentFilter(BaseFilterBackend):
--- apps/bear/views.py
+++ apps/bear/views.py
@@ -13,21 +13,20 @@ from .filters import *
 class KpiPerRestaurantView(ListAPIView):
     """
     [GET] time window와 그룹이름과 기간과 가격범위로 레스토랑별 총 매출 리턴
-     - params = "time_window", "number_of_party", "start_time", "end_time"
-                "start_price", "end_price", "Restaurant_name"
+     - params[필수] = "time_window", "start_time", "end_time"
+     - params[옵션] = "start_price", "end_price", "Restaurant_group"
     """
     queryset = Pos.objects.all()
     filter_backends = [KpiPerRestaurantFilter]
 
-    def _analyze_queryset(self, queryset):
-        # 필터링된 queryset을 ORM을 이용하여 분석하는 단계
-        # 베어로보틱스 문제의 핵심 Point
-        return 1
-
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
-        analyzed_data = self._analyze_queryset(queryset)
-        return Response(analyzed_data)
+        result = queryset.values(
+            'date',
+            'restaurant_id',
+            'total_price',
+            )
+        return Response(result)
 
 
 class KpiPerPaymentView(ListAPIView):
